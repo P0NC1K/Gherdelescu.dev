a7e3121c702c2471fc8e2cb2e06cc17b6f851516
Greg_Broflovski <ggherdelescu@mail.ru>
2023-05-03 13:54:31 +0300

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,29 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { HelmetProvider } from "react-helmet-async";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <HelmetProvider>
-      <Component {...pageProps} />
-    </HelmetProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-BF4TRGMPF0`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-BF4TRGMPF0', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+      <HelmetProvider>
+        <Component {...pageProps} />
+      </HelmetProvider>
+    </>
   );
 }
