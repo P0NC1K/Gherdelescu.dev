c4706cc39fd4266e6ef866979c54c8972d4f8c70
Greg_Broflovski <ggherdelescu@mail.ru>
2023-05-25 17:15:26 +0300

    Added Project & changed description

--- src/components/shared/Greeting.tsx
+++ src/components/shared/Greeting.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Greeting = (): React.ReactElement => {
   return (
     <div className="font-light">
-      I'm Gregory, a seasoned{" "}
-      <b className="font-bold">Full Stack Web Developer</b> and{" "}
+      I'm Gregory, a seasoned
+      <b className="font-bold"> Full Stack Web Developer</b> and{" "}
       <b className="font-bold">CTO</b>. I breathe life into digital projects
       with an impeccable blend of precision, passion, and professional
       excellence. I leverage my extensive expertise in Next.js, ReactJS, NodeJS,
